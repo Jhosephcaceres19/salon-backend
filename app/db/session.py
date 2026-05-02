79e1b3531a7ddab9700259fe2d7d5b9a7c6bda72
Jhoseph <jhoseph@zerocool.zerocool>
2026-05-02 02:06:50 -0400

    fix: db session

--- app/db/session.py
+++ app/db/session.py
@@ -2,5 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "options": "-c client_encoding=utf8"
+    }
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
